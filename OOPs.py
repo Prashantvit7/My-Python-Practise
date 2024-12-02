a068c4edcddc47a9c4c967525e3bf4858e0b474e
Prashant Maurya <prashantvit7@gmail.com>
2024-12-02 12:21:21 +0530

    OOP Question -Easy

---
+++ OOPs.py
@@ -0,0 +1,8 @@
+"""Write OOP classes to handle the following scenarios:
+
+- A user can create and view 2D coordinates
+- A user can find out the distance between 2 coordinates
+- A user can find find the distance of a coordinate from origin
+- A user can check if a point lies on a given line
+- A user can find the distance between a given 2D point and a given line"""
+print("OOP Program 1")
